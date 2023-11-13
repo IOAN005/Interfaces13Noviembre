86f3726c662885ce9f5c5d0a611e7778119774c7
IOAN005 <nellbovinca@gmail.com>
2023-11-13 18:10:23 +0100

    Initial commit

---
+++ src/com/corenetworks/presentacion/ProbarConductor.java
@@ -0,0 +1,18 @@
+package com.corenetworks.presentacion;
+
+import com.corenetworks.modelo.Coche1;
+
+public class ProbarVehiculo {
+    public static void main(String[] args) {
+
+    }
+    Coche1 c2=new Coche1(30);
+    int deposito;
+    if(deposito >=0){
+        System.out.println("el coche se puede mover ");
+    }else{
+        System.out.println("el coche no se mueve");
+    }
+
+
+}
